50eaae13db43b0ed4cad426fa12ec9810bf8e235
Austin Pierce <Austinentpierce@gmail.com>
2021-09-01 17:37:16 -0400

    all add and subtract buttons are displaying in console atleast for now working on getting the input to display in console

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import './style.css'
 
-let scoreCounter1 = 0
 let addingScore = document.querySelector('.team1 .add')
 
 function handleClickAddingScoreForTeamOne() {
-  console.log('event.target')
+  console.log('i did it')
 }
-const numberHeader1 = document
-scoreCounter1++
 addingScore?.addEventListener('click', handleClickAddingScoreForTeamOne)
+
+let subtractingScore1 = document.querySelector('.team1 .subtract')
+
+function handleClickSubtractingScoreForTeamOne() {
+  console.log('TOOK ONE AWAY')
+}
+subtractingScore1?.addEventListener(
+  'click',
+  handleClickSubtractingScoreForTeamOne
+)
 
 let addingScore2 = document.querySelector('.team2 .add')
 
@@ -16,3 +23,16 @@ function handleClickAddingScoreForTeamTwo() {
   console.log('OH YEAH WE CAME BACK')
 }
 addingScore2?.addEventListener('click', handleClickAddingScoreForTeamTwo)
+
+let subtractingScore2 = document.querySelector('.team2 .subtract')
+
+function handleClickSubtractingScoreForTeamTwo() {
+  console.log('TOOK AWAY')
+}
+subtractingScore2?.addEventListener(
+  'click',
+  handleClickSubtractingScoreForTeamTwo
+)
+
+const inputElementOne = document.querySelector('input')
+const HeaderTwoElement = document.querySelector('.team1 h2')
