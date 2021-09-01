e219b3c60a03d9fdc072ccf95b088cdcf3c0aaba
Austin Pierce <Austinentpierce@gmail.com>
2021-09-01 15:10:16 -0400

    working on adding numbers to the scoreboard after click is shown in

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
 import './style.css'
 
-const
+let scoreCounter1 = 0
+let addingScore = document.querySelector('.team1 .add')
+
+function handleClickAddingScoreForTeamOne() {
+  console.log('event.target')
+}
+const numberHeader1 = document
+scoreCounter1++
+addingScore?.addEventListener('click', handleClickAddingScoreForTeamOne)
+
+let addingScore2 = document.querySelector('.team2 .add')
+
+function handleClickAddingScoreForTeamTwo() {
+  console.log('OH YEAH WE CAME BACK')
+}
+addingScore2?.addEventListener('click', handleClickAddingScoreForTeamTwo)
